5517babcd01faba4c185d74decd887405342686e
Antonio Caceres <antonio.caceres178@gmail.com>
2023-02-21 00:11:53 -0500

    scatterplot 75% done

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ window.onload = function() {
     // create a function to draw the scatter plot
     function drawScatterPlot() {
         // set the dimensions and margins of the graph
-        var margin = {top: 10, right: 30, bottom: 30, left: 60},
-            width = 460 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+        var margin = {top: 50, right: 50, bottom: 50, left: 50},
+            width = 600 - margin.left - margin.right,
+            height = 550 - margin.top - margin.bottom;
     
         // append the svg object to the body of the page
         var svg = d3.select("#scatterplot")
@@ -16,25 +16,29 @@ window.onload = function() {
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
         .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
-    
+
+
         //Read the data
         d3.csv("../data/scatter-data.csv").then(function(data) {
 
+        const X_MAX = d3.max(data, d => {return parseInt(d.x)});
+        const Y_MAX = d3.max(data, d => {return parseInt(d.y)});
+
         // Add X axis
         var x = d3.scaleLinear()
-            .domain([0, 10])
-            .range([ 0, width]);
+            .domain([0, X_MAX])
+            .range([0, Y_MAX * 50]);
         svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
+            .attr("transform", "translate(" + 
+            margin.left + "," + (margin.top +height) + ")")
             .call(d3.axisBottom(x));
     
         // Add Y axis
         var y = d3.scaleLinear()
-            .domain([0, 10])
-            .range([ height, 0]);
+            .domain([0, Y_MAX])
+            .range([Y_MAX * 50 , 0]);
         svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
             .call(d3.axisLeft(y));
     
         // Add dots
@@ -43,8 +47,8 @@ window.onload = function() {
             .data(data)
             .enter()
             .append("circle")
-            .attr("cx", function (d) { return x(d.x); } )
-            .attr("cy", function (d) { return y(d.y); } )
+            .attr("cx", function (d) { return x(parseInt(d.x)) + margin.left; } )
+            .attr("cy", function (d) { return y(parseInt(d.y)) + margin.top } )
             .attr("r", 10)
             .style("fill", "deepskyblue")
             // add mouseover event listeners
@@ -64,13 +68,22 @@ window.onload = function() {
             // add click event listeners that records the x and y values and adds a border to the circle
             .on("click", function(d) {
                 d3.select(this)
-                .style("stroke-width", 5)
-                .style("stroke", "black");
+                d3.select(this).classed("highlightBorder", d3.select(this).classed("highlightBorder") ? false : true);
                 var lastClicked = d3.select("#point_select");
-                lastClicked.text("Last point clicked: X=" + d.x + ", Y=" + d.y);
-                
+                lastClicked.text("(" + (this.getAttribute("cx") / 50 - 1) + ", " + ((500- this.getAttribute("cy")) / 50) + ")");
                 });
+
+        //listen to html button click and add a new point
+        d3.select("#add_point").on("click", function() {
+            var x = d3.select("#x_input").node().value;
+            var y = d3.select("#y_input").node().value;
+            var point = {"x": x, "y": y};
+            data.push(point);
+            drawScatterPlot();
         });
+    });
+        //TODO: add tooltip add point selection
+
     }
     drawScatterPlot();
 
