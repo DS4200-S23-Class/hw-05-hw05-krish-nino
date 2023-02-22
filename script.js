c617f1a02ca1f9ce562520537056ec90953cf2d1
krishmoran <43583683+krishmoran@users.noreply.github.com>
2023-02-22 01:15:09 -0500

    added bar chart, tooltip

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-// create a scatter plot in d3 importing data from a csv file
-// using the d3.csv() method
-
-// create a function to draw the scatter plot
 
 
+// create a function to draw the scatter and bar plot when the page loads
 window.onload = function() {
     // create a function to draw the scatter plot
         // set the dimensions and margins of the graph
-        var margin = {top: 50, right: 50, bottom: 50, left: 50},
+        let margin = {top: 50, right: 50, bottom: 50, left: 50},
             width = 600 - margin.left - margin.right,
             height = 550 - margin.top - margin.bottom;
           
         // append the svg object to the body of the page
-        var svg = d3.select("#scatterplot")
+        let svg = d3.select("#scatterplot")
         .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
         .append("g")
 
-        var submit = d3.select("#submit");
+        let submit = d3.select("#submit");
 
 
         //Read the data
@@ -117,6 +114,102 @@ window.onload = function() {
 }
 
 document.getElementById("submit").addEventListener("click", addPoint);
+
+
+// add in barchart frame below scatterplot
+let bar = d3.select("#scatterplot").append("svg").attr("width", width).attr("height", height).append("g")
+
+function generateBarPlot() {
+    // create a function to draw the bar plot
+	d3.csv("data/bar-data.csv").then((data) => {
+
+        // get max y value according to data
+		const MAX_Y2 = d3.max(data, (d) => {return parseInt(d.amount); });
+
+        // create x-axis scaling value
+		const X_SCALE2 = d3.scaleBand()
+                        .domain(data.map((d) => {return d.category}))
+                        .range([0, width])
+                        .padding(0.2); 
+
+        // create y-axis scaling value                
+		const Y_SCALE2 = d3.scaleLinear()
+							.range([height, 0])
+							.domain([0, MAX_Y2])
+
+
+        // add bar chart to plot                     
+		bar.selectAll(".bar")
+						.data(data)
+		    			.enter().append("rect")
+		    						.attr("class", "bar")
+		    						.attr("x", d => {
+		    								return X_SCALE2(d.category) + margin.left
+		    							})
+		    						.attr("y", d => {
+		    							return (Y_SCALE2(d.amount) + margin.bottom)
+		    						})
+		    						.attr("width", X_SCALE2.bandwidth() - 5)
+		    						.attr("height", d => {
+		    							return (height - Y_SCALE2(d.amount))
+		    						}).style("fill", "deepskyblue")
+
+		// create x-axis
+		bar.append("g")
+	      		.attr("transform", "translate(" + 
+	      			margin.left+ "," + (margin.top + height) + ")")
+	      			.call(d3.axisBottom(X_SCALE2).tickFormat(function(d) { return d.category; })); 
+
+        // create y-axis
+		bar.append("g")
+	      		.attr("transform", "translate(" + 
+	      			margin.left + "," + (margin.top) + ")")
+	      		.call(d3.axisLeft(Y_SCALE2).ticks(10));
+
+	  
+        // create and add tooltip
+	    const bartooltip = d3.select("#scatterplot")
+							.append("div")
+								.attr("class", "tooltip")
+								.style("opacity", 0)
+                                .style("background-color", "white")
+
+
+		// add mouseover event listeners
+		function handleMouseover(event, d) {
+
+            // show tooltip when hovered over
+			bartooltip.style("opacity", 2);
+		}						
+
+	    // add mousemove event listeners
+		function handleMousemove(event, d) {
+
+            // update tooltip when it moves across
+			bartooltip.html("Name: " + d.category + "<br>Value: " + d.amount)
+					.style("left", (event.pageX + 10) + "px")
+					.style("top", (event.pageY - 50) + "px");
+		}
+
+        // add mouseleave event listeners
+		function handleMouseleave(event, d) {
+
+            // hide tooltip
+			bartooltip.style("opacity", 0);
+		}
+
+
+        // add event listeners to bars
+		bar.selectAll("rect")
+			.on("mouseover", handleMouseover)
+			.on("mousemove", handleMousemove)
+			.on("mouseleave", handleMouseleave);
+
+	});
+}
+
+// call function to generate bar plot when page loads
+generateBarPlot();
 
 
 }
